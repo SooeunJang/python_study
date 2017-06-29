cefee4c3a439edf92db7d453e75c7af66303d419
SooeunJang <allmysky7@gmail.com>
2017-06-29 16:23:18 +0900

    examples for python scripts

---
+++ example_char.py
@@ -0,0 +1,18 @@
+# print('jang sooeun example'.capitalize())
+# print('jang sooeun example'.upper())
+# print('jang sooeun example'.__len__())
+# print(len('jang sooeun example'))
+# print("jang sooeun example".replace('sooeun', 'Hg'))
+
+x = 5
+y = 10
+name = "jangsooeun"
+int_str = "123"
+float_str = "3.141592"
+print(name + x.__str__())
+print(int(int_str) + y)
+print(float(float_str))
+
+
+
+
