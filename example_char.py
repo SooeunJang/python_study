6e7518be168cfc0746764c39b2fdc51e55dcfe0e
SooeunJang <allmysky7@gmail.com>
2017-06-29 16:39:00 +0900

    Revert " revert "
    This reverts commit 821db065e943e29a6f8370e871b333efc01e0c09.

--- example_char.py
+++ example_char.py
@@ -1,15 +1,13 @@
-print('jang sooeun example'.capitalize())
-print('jang sooeun example'.upper())
-print('jang sooeun example'.__len__())
-print(len('jang sooeun example'))
-print("jang sooeun example".replace('sooeun', 'Hg'))
+# print('jang sooeun example'.capitalize())
+# print('jang sooeun example'.upper())
+# print('jang sooeun example'.__len__())
+# print(len('jang sooeun example'))
+# print("jang sooeun example".replace('sooeun', 'Hg'))
 
-print("this is first branch of my python example")
-
-x = 15
-y = 30
-name = "JangHg "
-int_str = "5678"
+x = 5
+y = 10
+name = "jangsooeun"
+int_str = "123"
 float_str = "3.141592"
 print(name + x.__str__())
 print(int(int_str) + y)
